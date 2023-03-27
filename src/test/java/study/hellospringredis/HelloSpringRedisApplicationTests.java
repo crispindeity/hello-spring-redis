53b3b71fde331f1a410beed13623cb66559deaa7
crispindeity <shoy1415@gmail.com>
2023-03-27 23:18:13 +0900

    :tada: project init

---
+++ src/test/java/study/hellospringredis/HelloSpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package study.hellospringredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
